2866cd423d451535df65fa7cce21942232a7a2a5
Kadim Arslanov <144515768+RyanGoslingIsNotDead@users.noreply.github.com>
2025-01-29 19:33:10 +0300

    Update v4-cju.sql

--- v4-cju.sql
+++ v4-cju.sql
@@ -1,3 +1,11 @@
-/**
-  @author
- */
+with cte as (
+             select * from author
+             where (extract(day from date_of_birth) between 1 and 15) and extract(year from date_of_birth) = 1970 and (extract(month from date_of_birth) between 1 and 6)
+             order by date_of_birth)
+select cte.first_name, cte.last_name, cte.date_of_birth, b.title
+        from cte
+join book_author as ba on cte.id = ba.author_id
+join book as b on ba.book_id = b.id
+union all
+select u.first_name, u.last_name, u.date_of_birth, null as title from "user" as u
+where (extract(day from date_of_birth) between 1 and 15) and (extract(month from date_of_birth) between 1 and 6);
